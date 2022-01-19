deffcb082863dd1a3d22d1f41913eb1122cbd4f1
Miriam Farkas <miriamjoshua00@gmail.com>
2022-01-19 21:29:38 +0200

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleTask(e.target.new-/task/-description.value)
+  })
 });
+
+function handleTask (todo) {
+ let list = document.createElement('li')
+list.innerText = todo
+document.querySelector('#tasks').appendChild(list)
+}
+
+
